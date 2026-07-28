655ce4a90bfdeb2835150608c22f403a2280a951
Heiner Jair Godoy Zamora <hainer2161@gmail.com>
2026-07-27 19:21:20 -0500

    midificacion entrypoint

--- front-end/entrypoint.sh
+++ front-end/entrypoint.sh
@@ -2,9 +2,9 @@
 set -eu
 
 API_BASE_URL="${API_BASE_URL:-/}"
-cat > /app/dist/nexus-agent-ui/assets/config.js <<EOF
+cat > /app/dist/nexus-agent-ui/browser/config.js <<EOF
 window.__APP_CONFIG__ = window.__APP_CONFIG__ || {};
 window.__APP_CONFIG__.apiBaseUrl = '${API_BASE_URL}';
 EOF
 
-npx serve -s /app/dist/nexus-agent-ui -l "${PORT:-4200}"
+npx serve -s /app/dist/nexus-agent-ui/browser -l "${PORT:-4200}"
